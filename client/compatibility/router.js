9ece390d62f0fde985caa2e9d348dd7f9f9e1133
Guillaume Morin <gmorin.pro@gmail.com>
2015-03-26 16:56:42 +0100

    fix undefined repository/user findOne result problem with iron:router

--- client/compatibility/router.js
+++ client/compatibility/router.js
@@ -1,4 +1,6 @@
 var testing = false; //Bad tricks to prevent the user undefined on direct access to /:username/:repo
+var testing_repo = false;
+
 Router.map(function () {
 
 	Router.configure({
@@ -37,27 +39,26 @@ Router.map(function () {
 			}
 			var repo = Repos.findOne({user_id: user._id, title: this.params.repo});
 			if (!repo) {
+				if (!testing_repo) {
+					testing_repo = true;
+					return false;
+				}
 				this.render('404');
 				return false;	
 			}
 
-			var file_length = repo.file_structure.length;
-			var nb_folder = 0, nb_files = 0;
-			for (var i = 0; i < file_length; i++) {
-				if (repo.file_structure[i].directory) {
-					nb_folder = nb_folder + 1;
-				} else {
-					nb_files = nb_files + 1;
-				}
-			}
-			
-			repo.nb_folder = nb_folder;
-			repo.nb_files = nb_files;
+			var history;
+			//  = Repos_history.findOne({user_id: user._id});
+			// if (!history) {
+			// 	console.log('render 404');
+			// 	this.render('404');
+			// 	return false;	
+			// }
 
-			return {repo: repo, user: user}
+			return {repo: repo, user: user, repo_history: history}
 		},
 		waitOn: function() {
-			return [Meteor.subscribe('users'), Meteor.subscribe('repos')];
+			return [Meteor.subscribe('users'), Meteor.subscribe('repos'), Meteor.subscribe('repos_history')];
 		}
 	});
 
