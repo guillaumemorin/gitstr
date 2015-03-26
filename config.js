3bc71372d70b87eddf5a60e2a083c7f041567fe1
Guillaume Morin <gmorin.pro@gmail.com>
2015-03-26 16:53:46 +0100

    add a default 404 image

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
-service_url = 'http://gitall.com/';
-service_git = 'git@gitall.com:';
+service_url = 'http://gitstr.com/';
+service_git = 'git@gitstr.com:';
+image_404 = '/noun_25237_cc.png';
 
 // Db
 Repos = new Mongo.Collection("repo");
+Repos_history = new Mongo.Collection("repo_history");
