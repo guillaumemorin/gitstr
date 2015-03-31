0648fa3365f1750faf6533880ef6aadec7fc1a8b
Guillaume Morin <gmorin.pro@gmail.com>
2015-03-31 17:44:10 +0200

    add dimmer message if  modal open fail + support video in the modal and cover_url

--- client/pages/partials/modals.js
+++ client/pages/partials/modals.js
@@ -5,17 +5,24 @@ Template.upload_modal.helpers({
 				console.log('fileinfo', fileInfo);
 
 				var tmp = Session.get('tmp_files') || [];
-				var type = fileInfo.type.split('/')[0] || '';
-				tmp.push({title: fileInfo.name, directory: 0, size: fileInfo.size, timestamp: new Date().getTime(), url: fileInfo.url, type: type});
+				var type = fileInfo.type.split('/') || [];
+				var subtype = type[0] || '';
+				var ext = type[1] || '';
+				var type_info = {subtype: subtype, ext: ext}
+
+				var path = fileInfo.path.split('/');
+
+				var cover_url = '/upload/' + path[0] + '/cover/' + path[1];
+				if (subtype === 'video') {
+					var name = fileInfo.name.split('.');
+					cover_url = '/upload/' + path[0] + '/' + name[0] + '/thumbnails.png';
+				}
+				
+				var url = '/upload/' + path[0] + '/' + path[1];
+
+				tmp.push({title: fileInfo.name, size: fileInfo.size, timestamp: new Date().getTime(), url: url, cover_url: cover_url, type: type_info});
 				Session.set('tmp_files', tmp);
 				Session.set('nb_tmp_files', Session.get('nb_tmp_files') + 1);
-
-				// Meteor.call('moveRepo', {id: Meteor.userId(), repo_id: Session.get('repo_id')}, fileInfo, function(error, result) {
-				// 	if (error) {
-				// 		console.log(error);
-				// 		Session.set("upload_error_message", error.reason);
-				// 	}
-				// });
 			}
 		}
 	},
@@ -24,16 +31,18 @@ Template.upload_modal.helpers({
 		return {
 			id: user_id
 		};
-	},
-	imageModalUrl: function() {
-		console.log('imageModalUrl')
-		return Session.get('image_modal_url');
 	}
 })
 
 Template.image_modal.helpers({
-	imageUrl: function() {
-		console.log('imageModalUrl')
-		return Session.get('image_modal_url');
+	modalInfo: function() {
+		console.log('modal_info', Session.get('modal_info'));
+		return Session.get('modal_info');
 	}
 })
+
+Template.image_modal.rendered = function () {
+	//Init
+	$('#image_modal').modal('setting', 'transition', 'fade up')
+};
+
