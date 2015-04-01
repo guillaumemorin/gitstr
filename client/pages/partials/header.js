14de37b6c86bf141f414f3bc31dfefdb3bc198d7
Guillaume Morin <gmorin.pro@gmail.com>
2015-04-01 23:35:01 +0200

    update topbar with scrolltop (disabled for now)

--- client/pages/partials/header.js
+++ client/pages/partials/header.js
@@ -13,3 +13,29 @@ Template.header.events({
 		Router.go('/');
 	}
 });
+
+// var $topbar = $('#topbar');
+// var $window = $(window);
+// var topbar_fixed = false;
+// $window.scroll(function() {
+	
+// 	if ($window.scrollTop() === 0) {
+// 		if (topbar_fixed) {
+// 			topbar_fixed = false;
+// 			$('#topbar').removeClass('fix');	
+// 		}
+// 		console.log('return topbar === 0')
+// 		return;
+// 	}
+
+// 	if (topbar_fixed) {
+// 		console.log('return topbar fixed')
+// 		return;
+// 	}
+
+// 	// if ($window.scrollTop() > 0) {
+// 		topbar_fixed = true;
+// 		$('#topbar').addClass('fix');
+// 	// }
+// 	console.log('scroll >>>>>>>>');
+// });
