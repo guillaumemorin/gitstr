c295822ef7a44e4a5650ac8a2977c677c02cc50c
Guillaume Morin <gmorin.pro@gmail.com>
2015-04-07 15:41:31 +0200

    split header and content grids

--- client/pages/partials/header.js
+++ client/pages/partials/header.js
@@ -14,28 +14,29 @@ Template.header.events({
 	}
 });
 
-// var $topbar = $('#topbar');
-// var $window = $(window);
-// var topbar_fixed = false;
-// $window.scroll(function() {
+var $topbar = $('#topbar');
+var $window = $(window);
+var topbar_fixed = false;
+$window.scroll(function() {
 	
-// 	if ($window.scrollTop() === 0) {
-// 		if (topbar_fixed) {
-// 			topbar_fixed = false;
-// 			$('#topbar').removeClass('fix');	
-// 		}
-// 		console.log('return topbar === 0')
-// 		return;
-// 	}
+	if ($window.scrollTop() === 0) {
+		if (topbar_fixed) {
+			topbar_fixed = false;
+			// $('#topbar').attr('class', 'ui secondary pointing menu');
+		}
+		console.log('return topbar === 0')
+		return;
+	}
 
-// 	if (topbar_fixed) {
-// 		console.log('return topbar fixed')
-// 		return;
-// 	}
+	if (topbar_fixed) {
+		console.log('return topbar fixed')
+		return;
+	}
 
-// 	// if ($window.scrollTop() > 0) {
-// 		topbar_fixed = true;
-// 		$('#topbar').addClass('fix');
-// 	// }
-// 	console.log('scroll >>>>>>>>');
-// });
+	// if ($window.scrollTop() > 0) {
+		topbar_fixed = true;
+		// $('#topbar').attr('class', 'ui fixed borderless primary menu');
+
+	// }
+	console.log('scroll >>>>>>>>');
+});
