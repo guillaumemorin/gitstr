161ea2cad10967680bf94084a0ef0539ff7aa06c
Guillaume Morin <gmorin.pro@gmail.com>
2015-04-08 15:38:40 +0200

    add dimmer and transition on subscriptions delete

--- client/pages/partials/repositories_grid.js
+++ client/pages/partials/repositories_grid.js
@@ -19,23 +19,31 @@ Template.repositories_grid.helpers({
 
 Template.repositories_grid.events({
 	'click .subscribe': function (event, template) {
-		console.log('id', event.target.dataset.id)
+
 		var repo_id = event.target.dataset.id;
+		var $card = template.$(event.target).parent().parent();
+		$card.dimmer('show');
+		window.setTimeout(function() {
+			$card.transition({
+				animation: 'fade right',
+				onComplete : function() {
+					Meteor.users.update(
+						{_id: Meteor.userId()},
+						{
+							$pull: {subscription: repo_id},
+							$inc: {nb_subscription: -1},
+						}
+					);
 
-		Meteor.users.update(
-			{_id: Meteor.userId()},
-			{
-				$pull: {subscription: repo_id},
-				$inc: {nb_subscription: -1},
-			}
-		);
-
-		Repos.update(
-			{"_id": repo_id},
-			{
-				$pull: {subscribers: Meteor.userId()},
-				$inc: {nb_subscribers: -1},
-			}
-		);	
+					Repos.update(
+						{"_id": repo_id},
+						{
+							$pull: {subscribers: Meteor.userId()},
+							$inc: {nb_subscribers: -1},
+						}
+					);	
+				}
+			});
+		}, 1000);
 	}
 });
