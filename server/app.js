4b54607649939fc4b15d537c8e0a1fdb167f39dc
Guillaume Morin <gmorin.pro@gmail.com>
2015-04-09 14:57:13 +0200

    upload and blur on the server the user avatar image get on services at user creation

--- server/app.js
+++ server/app.js
@@ -38,3 +38,75 @@ Meteor.startup(function () {
 		}
 	);
 });
+
+Accounts.onCreateUser(function(options, user) {
+
+	var exec = Npm.require('child_process').exec, http = Npm.require('http'), fs = Npm.require('fs');
+	var child, profile_image_url, profile_image, profile_image_mini;
+	var user_image_path = PROFILE_IMAGE_PATH + '/' + user._id;
+	var update_user_image = Meteor.bindEnvironment(function(err) {
+		
+		if (err) {
+			throw new Meteor.Error("http.get profile image-fail", "Something went wrong :(");
+		}
+
+		Meteor.users.update(
+			{_id: user._id},
+			{$set: {'profile.image': 'upload/public/u/' + user._id + '/avatar.jpg'}}
+		);
+	});
+
+	try {
+		fs.mkdirSync(user_image_path);
+	} catch(e) {
+		throw new Meteor.Error("onCreateUser-fail", "Something went wrong :(");
+	}
+
+	if (user.services.twitter) {
+		profile_image_url = user.services.twitter.profile_image_url;
+		profile_image = profile_image_url.replace('_normal', '_400x400');
+		profile_image_mini = profile_image_url.replace('_normal', '_mini');
+	}
+
+	if (user.services.facebook) {
+	}
+
+	if (user.services.github) {
+	}
+
+	if (profile_image) {
+
+		var request = http.get(profile_image, function(res) {
+			var imagedata = ''
+			res.setEncoding('binary')
+
+			res.on('data', function(chunk){
+				imagedata += chunk
+			})
+
+			res.on('end', function() {
+				fs.writeFile(user_image_path + '/avatar.jpg', imagedata, 'binary', function(err) {
+					update_user_image(err);
+				})
+			})
+		})
+
+		var imagemagick_command = 'convert -blur 0x5 ' + profile_image + ' ' + user_image_path + '/blurred.jpg'
+		child = exec(imagemagick_command, function (error, stdout, stderr) {
+			// console.log('stdout: ' + stdout);
+			// console.log('stderr: ' + stderr);
+			if (error !== null) {
+				console.log('exec error: ' + error);
+			}
+		});
+	}
+
+	if (options.profile) {
+		user.profile = options.profile;	
+		user.profile.image = profile_image;
+		user.profile.image_blur = 'upload/public/u/' + user._id + '/blurred.jpg';
+		// user.profile.image_url_mini = profile_image_mini;
+	}
+
+	return user;
+});
--- server/config.js
+++ server/config.js
@@ -1,5 +1,7 @@
 var path = Npm.require('path');
 
+
+
 FILES_PATH = path.resolve(process.env.PWD + '/../gitstr_files/');
 REPOSITORY_PATH = FILES_PATH + '/repos/';
 UPLOAD_PATH = FILES_PATH + '/uploads/';
@@ -7,3 +9,5 @@ UPLOAD_PATH = FILES_PATH + '/uploads/';
 NPM_PACKAGES_PATH = path.resolve(process.env.PWD + '/../gitstr_npm_packages/');
 NODEGIT_PATH = NPM_PACKAGES_PATH + '/nodegit';
 THUMBGEN_PATH = NPM_PACKAGES_PATH + '/thumbnails-webvtt';
+
+PROFILE_IMAGE_PATH = UPLOAD_PATH + '/public/u';
