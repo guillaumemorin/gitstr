7a51de8a57530f41026e0db68c3868bd600759d7
Guillaume Morin <gmorin.pro@gmail.com>
2015-04-15 16:14:01 +0200

    major changes on repositories_grid partial

--- client/js/pages/partials/repositories_grid.js
+++ client/js/pages/partials/repositories_grid.js
@@ -1,30 +1,73 @@
+Template.repositories_grid.helpers({
+	repoList: function () {
+		var data = UI.getData();
+		var list = data.repo;
+		var display = data.display;
+
+		if (!data.repo.count() && display === 'sub') {
+			list = Repos.find({user_id: { $not: Meteor.userId()}}, {limit: 50});
+		}
+		return list;
+	}
+});
+
 Template.repositories_grid.events({
 	'click .subscribe': function (event, template) {
 
-		var repo_id = event.target.dataset.id;
-		var $card = template.$(event.target).parent().parent();
-		$card.dimmer('show');
-		window.setTimeout(function() {
-			$card.transition({
-				animation: 'fade right',
-				onComplete : function() {
-					Meteor.users.update(
-						{_id: Meteor.userId()},
-						{
-							$pull: {subscription: repo_id},
-							$inc: {nb_subscription: -1},
-						}
-					);
+		if (!Meteor.user()) {
+			$('#signin')
+				.modal('setting', 'transition', 'fade up')
+				.modal('show');
+			return;
+		}
 
-					Repos.update(
-						{"_id": repo_id},
-						{
-							$pull: {subscribers: Meteor.userId()},
-							$inc: {nb_subscribers: -1},
-						}
-					);	
-				}
-			});
-		}, 1000);
+		var repo_id = event.target.dataset.id;
+		var repo = Repos.findOne({_id: repo_id}, {fields: {subscribers: 1}});
+		var subscribers = repo.subscribers;
+
+		if (~_.indexOf(subscribers, Meteor.userId())) {
+
+			var $card = template.$(event.target).parent().parent();
+			$card.dimmer('show');
+			window.setTimeout(function() {
+				$card.transition({
+					animation: 'fade right',
+					onComplete : function() {
+						Meteor.users.update(
+							{_id: Meteor.userId()},
+							{
+								$pull: {subscription: repo_id},
+								$inc: {nb_subscription: -1},
+							}
+						);
+
+						Repos.update(
+							{"_id": repo_id},
+							{
+								$pull: {subscribers: Meteor.userId()},
+								$inc: {nb_subscribers: -1},
+							}
+						);	
+					}
+				});
+			}, 1000);
+			return;
+		}
+
+		Meteor.users.update(
+			{_id: Meteor.userId()},
+			{
+				$addToSet: {subscription: repo_id},
+				$inc: {nb_subscription: 1},
+			}
+		);
+
+		Repos.update(
+			{"_id": repo_id},
+			{
+				$addToSet: {subscribers: Meteor.userId()},
+				$inc: {nb_subscribers: 1},
+			}
+		);
 	}
 });
