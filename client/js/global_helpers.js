0169c6ca3a8f9684bea08a5fc5eb35eac8ca40aa
Guillaume Morin <gmorin.pro@gmail.com>
2015-04-30 22:21:39 +0200

    remove console.log on global_helpers.js

--- client/js/global_helpers.js
+++ client/js/global_helpers.js
@@ -19,7 +19,6 @@ Template.registerHelper('setLastUpdate', function(timestamp) {
 });
 
 Template.registerHelper('isOwner', function(repo_owner_id) {
-	console.log(repo_owner_id, Meteor.userId());
 	return repo_owner_id === Meteor.userId();
 });
 
